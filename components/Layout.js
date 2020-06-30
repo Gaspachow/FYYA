e400c01afe91828fec0a8ae1c0741dc8841b7c8c
gaspachow <gaspardsmets@gmail.com>
2020-06-30 15:34:55 +0200

    footer + navbar details

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,25 @@
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
-import Image from "react-bootstrap/NavDropdown"
+import Container from "react-bootstrap/Container"
 
 function Header() {
 
   const navStyle = {
-    backgroundColor: "#0b56a2"
+    backgroundColor: "#0b56a2",
   }
 
   const imgStyle = {
     maxHeight: "80px",
     maxWidth: "100%"
+  }
+
+  const navItem = {
+    paddingRight: "30px"
+  }
+
+  const navDropItem = {
+    paddingRight: "20px"
   }
 
     return(
@@ -25,28 +33,77 @@ function Header() {
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto ml-auto">
-        <Nav.Link href="#home">ACTUALITÉS</Nav.Link>
-        <Nav.Link href="#link">ADHÉRER</Nav.Link>
-        <NavDropdown bg="primary" variant="dark" title="RESSOURCES" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+      <Nav className="ml-auto">
+        <Nav.Link style={navItem} href="#home">ACTUALITÉS</Nav.Link>
+        <Nav.Link style={navItem} href="#link1">ADHÉRER</Nav.Link>
+        <NavDropdown style={navDropItem} bg="primary" variant="dark" title="RESSOURCES" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Apprendre le yoyo</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Boutiques en ligne</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">À l’étranger</NavDropdown.Item>
         </NavDropdown>
-        <NavDropdown title="ARCHIVES" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown style={navDropItem} title="ARCHIVES" id="basic-nav-dropdown2">
+          <NavDropdown.Item href="#action/4.1">Championnats précédents</NavDropdown.Item>
+          <NavDropdown.Item href="#action/4.2">Assemblées générales</NavDropdown.Item>
+          <NavDropdown.Item href="#action/4.3">Packs FYYA</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#link">À PROPOS</Nav.Link>
-        <Nav.Link href="#link">CONTACT</Nav.Link>
+        <Nav.Link style={navItem} href="#link2">À PROPOS</Nav.Link>
+        <Nav.Link style={navItem} href="#link3">CONTACT</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
     )
 }
 
-export default Header
+function Footer() {
 
-// ACTUALITÉS ADHÉRER RESSOURCES ARCHIVES À PROPOS CONTACT
-// Association Française de Yoyo©
+  const footStyle = {
+    backgroundColor: "#0b56a2",
+    color: "#AAA",
+    height: "60px"
+  }
+
+  const socialStyle = {
+      padding: "10px",
+      textAlign: "center",
+      textDecoration: "none",
+      margin: "5px 2px",
+      borderRadius: "50%",
+      fontSize: "30px",
+      color: "white"
+    }
+
+  return(
+    <Navbar style={footStyle} className="sticky-bottom">
+      <Navbar.Brand href="#home">
+          <img 
+          src="/Icone-FYYA.png"
+          style={{maxHeight: "50px"}}
+          />
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-start">
+        <Navbar.Text style={{color: "#AAA", fontSize: "min(2vw, 0.875em)"}}>
+        © 2020 Association Française de Yoyo. All rights reserved.
+        </Navbar.Text>
+        <a href="https://www.facebook.com/franceyoyoassociation/" style={socialStyle} class="view overlay zoom ml-auto fa fa-facebook"></a>
+        <a href="https://www.youtube.com/channel/UCHBBzg5p5zuAMe9vD01fX8w/featured" style={socialStyle} class="view overlay zoom fa fa-youtube"></a>
+        <a href="https://vimeo.com/user19541937" style={socialStyle} class="view overlay zoom fa fa-vimeo"></a>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+
+function Layout() {
+  return(
+    <>
+    <div style={{minHeight: "calc(100vh - 60px)"}}>
+      <Header/>
+    </div>
+      <Footer/>
+    </>
+  )
+}
+
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "../node_modules/font-awesome/css/font-awesome.min.css"
+import "../styles/custom.css"
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
