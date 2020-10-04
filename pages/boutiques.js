42fa6a21c3c379d1763fa74a2851be7882b3cee7
gaspachow <gaspardsmets@gmail.com>
2020-10-04 20:28:31 +0200

    boutiques page finish (images + links + details)

--- pages/boutiques.js
+++ pages/boutiques.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import Container from "react-bootstrap/Container"
 import ImgTitle from "../components/ImgTitle"
 import Image from "react-bootstrap/Image"
+import Link from "next/link"
 
 const content = {
   tabTitle: "Boutiques en ligne",
@@ -12,17 +13,25 @@ const content = {
   thirdTitle: "Asie"
 }
 
-function StoreImg({storeName}) {
+function StoreImg({storeImg, storeLink, border}) {
 
+  if (!storeLink) {
+    storeLink="#";
+  }
+  if (!border) {
+    border = "0px";
+  }
   return(
     <Container 
-      className="d-flex justify-content-center hoverItem"
-      style={{marginBottom: 20, flexBasis: "30%"}}
+      className="d-flex justify-content-center align-items-center hoverItem"
+      style={{minHeight: "100px", marginBottom: 20, flexBasis: "30%"}}
     >
-      <Image
-        src={"/pages-img/boutiques/" + storeName + ".jpg"}
-        style={{width: "200px", margin: "auto"}}
-      />
+      <a href={storeLink} target="_blank">
+        <Image
+          src={"/pages-img/boutiques/" + storeImg}
+          style={{width: "200px", margin: "auto", borderRadius: border}}
+        />
+      </a>
     </Container>
   )
 }
@@ -34,28 +43,30 @@ export default function Home() {
       <Head>
         <title>{content.tabTitle}</title>
       </Head>
-      <ImgTitle imgUrl="/pages-img/adherer.jpg" imgTitle={content.imageTitle} imgHeight="30vh"/>
+      <ImgTitle imgUrl="/pages-img/boutiques.jpg" imgTitle={content.imageTitle} imgHeight="30vh"/>
       <Container style={{marginBottom: 30, marginTop: 30}}>
         <p className="h1 text-center p-4">{content.firstTitle}</p>
         <Container className="d-flex flex-wrap justify-content-center">
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
+          <StoreImg storeLink="https://www.yoyoblast.com/" storeImg="yoyoblast.png"/>
+          <StoreImg storeLink="https://www.yoyoworld.dk/en/" storeImg="yoyoworld.png"/>
+          <StoreImg storeLink="https://www.slusny.net/" storeImg="slusny.png"/>
+          <StoreImg storeLink="https://www.latiendadelyoyo.com/" storeImg="latienda.jpg"/>
+          <StoreImg storeLink="http://www.yoyovillage.co.uk/" storeImg="yoyovillage.jpg"/>
+          <StoreImg storeLink="https://www.yoyoraven.com/" storeImg="yoyoraven.png"/>
         </Container>
+        <hr></hr>
         <p className="h1 text-center p-4">{content.secondTitle}</p>
         <Container className="d-flex flex-wrap justify-content-center">
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
+          <StoreImg border="15px" storeLink="https://www.yoyoexpert.com/" storeImg="yoyoexpert.jpg"/>
+          <StoreImg storeLink="https://usa.yoyostorerewind.com/" storeImg="rewindusa.png"/>
+          <StoreImg storeLink="https://www.yoyosam.com/" storeImg="yoyosam.jpg"/>
         </Container>
+        <hr></hr>
         <p className="h1 text-center p-4">{content.thirdTitle}</p>
         <Container className="d-flex flex-wrap justify-content-center">
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
-          <StoreImg storeName="latienda"/>
+          <StoreImg storeLink="https://spingear.jp/" storeImg="spingear.png"/>
+          <StoreImg storeLink="https://www.yoyostorerewind.com/" storeImg="rewindworld.png"/>
+          <StoreImg border="15px" storeLink="https://www.spinworkx.com/" storeImg="spinworkx.png"/>
         </Container>
       </Container>
     </Layout>
